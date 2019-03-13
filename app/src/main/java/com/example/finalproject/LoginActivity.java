e210728dbf23fba2896e76141b4e831715fc5262
Alec Baird <alec40baird@gmail.com>
2019-03-13 13:29:16 -0600

    Set up Logging

--- app/src/main/java/com/example/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
     /**
      * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
+     * TODO: remove after connecting to a real authentication system. firebase
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
             "foo@example.com:hello", "bar@example.com:world"
