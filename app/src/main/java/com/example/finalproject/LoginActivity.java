afa07f0824c55ada4380ef5e97cf8d29ddc18d91
Alec Baird <alec40baird@gmail.com>
2019-03-21 11:12:06 -0600

    Added new account creation

--- app/src/main/java/com/example/finalproject/LoginActivity.java
+++ app/src/main/java/com/example/finalproject/LoginActivity.java
@@ -206,12 +206,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     }
 
     private boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@");
     }
 
     private boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 
@@ -343,6 +341,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
 
             // TODO: register the new account here. ALEC
+            String newCredential = mEmail + ":" + mPassword;
+            //Store through Joseph's DB Connection
             return true;
         }
 
