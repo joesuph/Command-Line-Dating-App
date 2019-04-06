1852a077e502c3b7de82f729fc0fb5ad675fc7b7
Joseph <joseph.olsen98@yahoo.com>
2019-04-06 12:05:04 -0600

    UI Setup to display results

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -5,8 +5,11 @@ import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
@@ -52,15 +55,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-        public void process(String args){
-            updateUI(args,"Error: Arguments not valid");
+        public void process(String args) {
+
+            updateUI(args,"");
         }
 
         public void updateUI(String input, String result){
             LinearLayout llviews = (LinearLayout) findViewById(R.id.linear_layout);
-            TextView tvText = new TextView(this);
+            TextView tvText = new TextView(new ContextThemeWrapper(this, R.style.terminalTextView), null, 0);
 
+            //Set margin of textview
+            float dpRatio = this.getResources().getDisplayMetrics().density;
+            LinearLayout.LayoutParams params= new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            params.setMargins((int)(20 * dpRatio), (int)(8 * dpRatio), 0, (int)(15 * dpRatio));
+            tvText.setLayoutParams(params);
 
+            tvText.setText(input + result);
+            llviews.addView(tvText);
+            EditText et = (EditText) findViewById(R.id.editText);
+            et.setText("");
+            llviews.removeView(et);
+            llviews.addView(et);
         }
 
 
