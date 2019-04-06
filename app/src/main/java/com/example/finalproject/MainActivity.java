5cb1577dc51ddce8be759025d7e13835328bf439
Joseph <joseph.olsen98@yahoo.com>
2019-04-06 13:39:27 -0600

    added help and getprofiles command

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,19 +1,14 @@
 package com.example.finalproject;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.KeyEvent;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -56,8 +51,39 @@ public class MainActivity extends AppCompatActivity {
         });
     }
         public void process(String args) {
+            String result = "";
 
-            updateUI(args,"");
+            if(args.equals("cat profiles.json")){
+                getProfiles();
+                return;
+            }
+
+            args.split(" ");
+            switch(args)
+            {
+                case "":
+                    result = "";
+                    break;
+                case "help":
+                    result += "\n\t\t>>Type \"cat profiles.json\" to see profiles";
+                    result += "\n\t\t>>Type \"ping <userid>\" to like a user";
+                    result += "\n\t\t>>Type \"grep 'likesMe' profiles.json\" to see who you liked you back";
+                    break;
+            }
+            updateUI(args,result);
+        }
+
+        public void getProfiles(){
+            String result = "\n\t{";
+            for (Map<String,Object> p : profiles){
+                result += "\n\t\t{";
+                result += "\n\t\t\tuserId: \"" + p.get("userId") + "\"";
+                result += "\n\t\t\tname: \"" + p.get("name") + "\"";
+                result += "\n\t\t\tdescription: \"" + p.get("description") + "\"";
+                result += "\n\t\t}";
+            }
+            result += "\n\t}";
+            updateUI("cat profiles.json",result);
         }
 
         public void updateUI(String input, String result){
@@ -76,6 +102,11 @@ public class MainActivity extends AppCompatActivity {
             et.setText("");
             llviews.removeView(et);
             llviews.addView(et);
+
+            ScrollView sv = (ScrollView)findViewById(R.id.scroll_view);
+            sv.scrollTo(0, sv.getBottom());
+            et.setFocusableInTouchMode(true);
+            et.requestFocus();
         }
 
 
