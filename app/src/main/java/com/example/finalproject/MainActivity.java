6bdc841d4ecca9add213ef782267a7652eb59f63
Joseph <joseph.olsen98@yahoo.com>
2019-04-06 10:44:08 -0600

    added styles for editText and textView for console components

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.finalproject;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,7 +24,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+
+
         Intent intent = getIntent();
         String message = intent.getStringExtra(LoginActivity.EXTRA_MESSAGE);
         String[] arr = message.split(":", 2);
@@ -26,7 +36,34 @@ public class MainActivity extends AppCompatActivity {
 
         GetAccountInfoTask task = new GetAccountInfoTask(email, password, dbit, this);
         task.execute((Void) null);
+
+        //When the user clicks enter after typing in console
+        final EditText edittext = (EditText) findViewById(R.id.editText);
+        edittext.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    process(edittext.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
     }
+        public void process(String args){
+            updateUI(args,"Error: Arguments not valid");
+        }
+
+        public void updateUI(String input, String result){
+            LinearLayout llviews = (LinearLayout) findViewById(R.id.linear_layout);
+            TextView tvText = new TextView(this);
+
+
+        }
+
+
 
 
 
